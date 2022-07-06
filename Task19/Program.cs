fbd4fed3e59d3bfa8bad36bac46cf4fa72b5720c
Nikita Balenko <nikitabalenko2013@gmail.com>
2022-07-06 15:51:49 +0300

    Выпонил все задания

---
+++ Task19/Program.cs
@@ -0,0 +1,33 @@
+﻿// Задача 19
+// Напишите программу, которая принимает на вход пятизначное число и проверяет, является ли оно палиндромом.
+// 14212 -> нет
+// 12821 -> да
+// 23432 -> да
+
+System.Console.WriteLine("Введите пятизначное число");
+int num = Convert.ToInt32(Console.ReadLine());
+int[] arr = new int[5];
+if (num < 100000 && num > 9999)
+{
+    int i = 0;
+    while (i < 5)
+    {
+        arr[i] = num % 10;
+        i++;
+        num/=10;
+    }
+
+
+    if (arr[0] == arr[4] && arr[1] == arr[3])
+    {
+        System.Console.WriteLine("да");
+    }
+    else
+    {
+        System.Console.WriteLine("нет");
+    }
+}
+else
+{
+    System.Console.WriteLine("Введено неправильное число");
+}
---
+++ Task21/Program.cs
@@ -0,0 +1,13 @@
+﻿// Задача 21
+// Напишите программу, которая принимает на вход координаты двух точек и находит расстояние между ними в 3D пространстве.
+// A (3,6,8); B (2,1,-7), -> 15.84
+// A (7,-5, 0); B (1,-1,9) -> 11.53
+
+
+int[] A = new int[3]{3, 6, 8};
+int[] B = new int[3]{2, 1, -7};
+
+double answer = Math.Pow((A[0] - B[0]), 2) + Math.Pow((A[1] - B[1]), 2) + Math.Pow((A[2] - B[2]), 2);
+answer = Math.Pow(answer, 0.5);
+answer = answer - (answer % 0.01);
+System.Console.WriteLine(answer);
